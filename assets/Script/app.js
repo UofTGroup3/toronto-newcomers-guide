22bb7eaa1bbacccfa5d16931a0b21debde1c56c3
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-08-19 18:52:46 -0400

    feat(login): create a function for user login attempt

--- assets/Script/app.js
+++ assets/Script/app.js
@@ -1,3 +1,4 @@
+// Used(https://learn.jquery.com/using-jquery-core) and (https://materializecss.com) documentations as reference.
 $(document).ready(function () {
 
   var userBtn = $('#userBtn');
@@ -7,6 +8,10 @@ $(document).ready(function () {
   var profileType = $('#profileType');
   // Used(https://learn.jquery.com/using-jquery-core/selecting-elements/) as reference.
   var modalFooterLogin = $('.modal-footer a:contains("Login")');
+  var username = $('#username');
+  var password = $('#password');
+  var loginModal = $('#loginModal');
+  var loginError = $('#loginError');
 
   // Initialize dropdown, modals and select
   userBtn.dropdown();
@@ -14,9 +19,31 @@ $(document).ready(function () {
   select.formSelect();
 
   var newProfileType = function () {
+
     var selectedProfile = profileType.val();
     localStorage.setItem('profileType', selectedProfile);
+
   }
   profileType.change(newProfileType);
+
+  window.attemptLogin = function () {
+
+    var loginUsername = username.val();
+    var loginPassword = password.val();
+
+    var storedUsername = localStorage.getItem('username');
+    var storedPassword = localStorage.getItem('password');
+
+    if (loginUsername === storedUsername && loginPassword === storedPassword) {
+      //using the jQuery method for closing modals with Materialize CSS.
+      loginModal.modal('close');
+    } else {
+      loginError.show();
+    }
+  };
+
+  modalFooterLogin.click(function () {
+    attemptLogin();
+  });
 
 });
