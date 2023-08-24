a81ec8bbbc72ada694bddb9be3229d83086ac1ca
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-08-23 21:46:24 -0400

    feature: added postoffice/libraries and police/fire station maps. Added description in html about the maps. Added buttons for the rest of the maps. also included some back up translator codes in comments since it is being glitchy, will delete later

--- assets/Script/app.js
+++ assets/Script/app.js
@@ -196,20 +196,24 @@ $(document).ready(function () {
   $('.mapFilter').click(function () {
   var filter = $(this).data("filter");
   //debug
-  console.log("clicked filter:", filter);
+  // console.log("clicked filter:", filter);
   //Hide all maps on click
   hideMaps();
   //Show clicked on map
   $("#" + filter).show();
   });
 
-    // https://www.javatpoint.com/how-to-add-google-translate-button-on-your-webpage#:~:text=translator%20api%20%2D%2D%3E-,%3Cscript%20type%3D%22text%2Fjavascript%22,will%20be%20translated
+    // // https://www.javatpoint.com/how-to-add-google-translate-button-on-your-webpage#:~:text=translator%20api%20%2D%2D%3E-,%3Cscript%20type%3D%22text%2Fjavascript%22,will%20be%20translated
 
     function googleTranslateElementInit() {
       new google.translate.TranslateElement({ pageLanguage: 'en', 
       includedLanguages: 'en,es,fr,de,af,sq,ar,bs,bg,hy,zh-CN,hr,cs,da,nl,el,gu,he,hi,hu,it,ja,ko,fa,pl,pt,pa,ro,ru,sr,so,sv,ta,th,tr,uk,ur,vi,zu', 
       layout: google.translate.TranslateElement.InlineLayout.SIMPLE }, 'google_translate_element');
       };
-    
+    // // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_google_translate
+    // function googleTranslateElementInit() {
+    //   new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+    // };
+    // googleTranslateElementInit();
 
 });
